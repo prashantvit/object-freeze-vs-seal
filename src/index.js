b76a4d020ce9cdbd8a4720e1c0e20624a01914c4
Prashant Choudhary <prashant.srs@gmail.com>
2018-12-14 23:34:44 +0530

    isFrozen and isSealed addition

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ var marks = { physics: 98, maths: 95, chemistry: 91 };
 finalizedMarks = Object.freeze(marks);
 finalizedMarks["physics"] = 86; // throws error in strict mode
 console.log(marks); // {physics: 98, maths: 95, chemistry: 91}
+Object.isFrozen(finalizedMarks); // returns true
 
 var marks = { physics: 98, maths: 95, chemistry: 91 };
 Object.seal(marks);
 delete marks.chemistry; // returns false as operation failed
 marks.physics = 95; // Works!
 marks.greek = 86; // Will not add a new property
+Object.isSealed(marks); // returns true
